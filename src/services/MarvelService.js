5c041a511042fed88290dd2f94fd74d708ae64c5
Dmytro <ccccuxoy@gmail.com>
2022-05-11 14:58:59 +0300

    randomCharacterAdded

---
+++ src/services/MarvelService.js
@@ -0,0 +1,37 @@
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=53fa4d2285b861097b61ec23f4943d33'
+    getResource = async (url) =>{
+        let res = await fetch(url)
+
+        if(!res.ok){
+            throw new Error(`could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+
+    getAllCharacters = async () =>{
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=415&${this._apiKey}`)
+        return res.data.results.map(this._tranformCharacter)
+    }
+
+    getCharacter = async (id) =>{
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._tranformCharacter(res.data.results[0])
+
+    }
+
+    _tranformCharacter = (char) =>{
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension ,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+export default MarvelService;
